686a3908f6ec006ff4342f63b41d719df2ae4661
Andrew <im@andreystarkov.ru>
2013-08-06 22:53:07 +0600

    ive made it without your help, tsvetkovpro

--- js/engine.js
+++ js/engine.js
@@ -1,11 +1,67 @@
 $(function () {
 
-/* rgba(0,152,114, 1); */
-/* rgba(233,242,241, 1); */
-/* rgba(0,152,114, 1); */
-/* rgba(41,101,156, 1); */
-/* rgba(255,194,84, 1); */
-/* rgba(199,66,56, 1); */
+ $.cookie('sent', '');
+  if($.cookie('sent') == '1'){
+      $('#button-send').html('<i class="icon-ok-circle icon-large"></i> Ваша заявка принята');
+      $('#button-send').animate({color: 'rgba(255,255,255,1)'});
+      $('#button-send').animate({backgroundColor: '#93cb5d', borderBottomColor: '#618d37', boxShadow: 'inset 0px 0px 0px 1px #618d37, inset 0px 2px 1px 0px rgba(255,255,255,0.75)', color: 'rgba(255,255,255,0)'});
+    }
+
+
+
+  $('#button-send').click( function(){
+    var name = $('#name').val();
+    var email = $('#email').val();
+    var text = $('textarea#message').val();
+
+      if($.cookie('sent') != '1'){
+
+        if(email == ""){
+          $('.tt-warning').tooltipster('enable');
+          $('.tt-warning').tooltipster('show');
+        } else {
+
+          $.ajax({
+            type: 'POST',
+            url: 'mail.php',
+            data: {
+              'name': name,
+              'email': email,
+              'text': text
+            },
+
+            success: function(msg){
+              $.cookie('sent', '1',{ expires: 1});
+              $('.tt-sent').tooltipster('enable');
+              $('.tt-sent').tooltipster('show');
+              $('#button-send').animate({backgroundColor: '#93cb5d', borderBottomColor: '#618d37', boxShadow: 'inset 0px 0px 0px 1px #618d37, inset 0px 2px 1px 0px rgba(255,255,255,0.75)', color: 'rgba(255,255,255,0)'},
+              function(){
+              $('#button-send').html('<i class="icon-ok-circle icon-large"></i> Ваша заявка принята');
+              $('#button-send').animate({color: 'rgba(255,255,255,1)'});
+              });
+            }
+          });
+      }
+      } else {
+         $('.tt-sent').tooltipster('enable');
+         $('.tt-sent').tooltipster('update', '<i class="icon-warning-sign icon-4x tt-ico"></i><span class="tt-txt">Вы уже отправили одну заявку несколько минут назад. Чтобы отправить ещё одну с этого же компьютера &mdash; подождите несколько минут. </span>');
+         $('.tt-sent').tooltipster('show');
+      }
+
+  });
+
+    $('.tt-sent').tooltipster({
+       animation: 'grow',
+       content:  '<i class="icon-ok-circle tt-ico icon-4x"></i><span class="tt-txt">Спасибо за обращение! Ваша заявка была отправлена специалисту. Он свяжется с вами, удобным для Вас способом в кратчайшие сроки.</span>',
+       position: 'top', theme: '.tooltipster-punk', maxWidth: 510, trigger: 'click' });
+  $('.tt-sent').tooltipster('disable');
+
+    $('.tt-warning').tooltipster({
+       animation: 'grow',
+       content:  'Вы должны ввести контакную информацию (E-Mail или номер телефона), что бы наш менеджер мог связаться с Вами.',
+       position: 'bottom', theme: '.tooltipster-punk', maxWidth: 260, trigger: 'click' });
+  $('.tt-warning').tooltipster('disable');
+
     $('.get-down').tooltipster({
        animation: 'grow',
        content:
---
+++ mail.php
@@ -0,0 +1,63 @@
+<?php
+
+// Change these two variables to meet your needs.
+
+$myemail = 'im@andreystarkov.ru';
+$subject = 'webscapes.ru';
+
+
+    $name = stripslashes($_POST[name]);
+    $email = stripslashes($_POST[email]);
+    $text = stripslashes($_POST[text]);
+    $referer = $_POST[referer];
+    $remote_host = $_SERVER[REMOTE_ADDR];
+    $server = $_SERVER[SERVER_NAME];
+    $browser = $_SERVER[HTTP_USER_AGENT];
+
+
+    if(!$email)
+    {
+        $status .= "No back.<br>";
+    }
+
+setcookie("nospam", "", time()-3600);
+
+if($_COOKIE['nospam'] == ""){
+    if(!$status)
+    {
+        $header = "From: $email\r\nReply-To: $email\r\n";
+
+		$message = "
+		Имя отправителя: $name
+		Обратная связь: $email
+
+		$text
+
+		---
+
+		IP: $remote_host
+		CL: $browser
+		";
+
+        if(mail($myemail, $subject, $message, $header)){
+            $status = "Thank you for your Feedback!!<br><br>";
+			setcookie("nospam", $remote_host, time()+500);
+        }
+
+    }
+	$current = "<br><br>name: ".$name."<br>back: ".$email."<br><br>text: ".$text."<br><br><br>current: ".$current."<br><br><hr>".$_COOKIE['sent'];
+
+} else
+
+$current = "Cookie = ".$_COOKIE['nospam'];
+$file = 'debug.html';
+
+file_put_contents($file, $current);
+
+
+$referer = $_SERVER[HTTP_REFERER];
+
+if(!preg_match('#^http://[a-z0-9-]+.([a-z0-9-]+.)?[a-z]+#i', $referer))
+{
+    unset($referer);
+}
